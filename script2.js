b5a8f7542f1af4f3495bb861959b7178f2a8d7f7
henri-kapidani <h.kapidani@gmail.com>
2023-10-11 20:11:59 +0400

    trying to fix the datachannel not opening

--- script2.js
+++ script2.js
@@ -4,57 +4,64 @@ const btnStart = document.querySelector('.start');
 const eleCode = document.querySelector('.code');
 
 const options = {
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+	iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
 };
 
 const pc = new RTCPeerConnection(options);
-const channel = pc.createDataChannel('moves', {negotiated: true, id: 0});
-channel.onopen = e => channel.send('open');
-channel.onmessage = e => console.log(e.data);
+const channelData = pc.createDataChannel('channelData', {
+	negotiated: true,
+	id: 1,
+});
+
+// let channel;
+channelData.onopen = e => console.log('open');
+channelData.onmessage = e => console.log(e.data);
+
+// channelData.send('message');
 
 if (searchParams.has('o')) {
-    btnShare.style.display = 'none';
-    const offer = JSON.parse(searchParams.get('o'));
+	const offer = JSON.parse(searchParams.get('o'));
 
-    // pc.ondatachannel = e => {
-    //     e.channel.onopen = e => console.log("open");
-    //     e.channel.onmessage = e => console.log(e.data);
-    //     e.channel.onclose = e => console.log("closed");
-    //     pc.channel = e.channel;
-    // }
+	// pc.ondatachannel = e => {
+	// 	console.log('ondatachannel');
+	// 	channelData = e.channelData;
+	// 	channelData.onopen = e => console.log('open');
+	// 	channelData.onmessage = e => console.log(e.data);
+	// 	channelData.onclose = e => console.log('closed');
+	// };
 
-    pc.setRemoteDescription(offer)
-        .then(a => console.log("done"));
+	pc.setRemoteDescription(offer).then(a => console.log('done'));
 
-    //create answer
-    pc.createAnswer()
-        .then(a => pc.setLocalDescription(a))
-        .then(a => console.log(JSON.stringify(pc.localDescription)));
-    //send the anser to the client 
+	//create answer
+	pc.createAnswer()
+		.then(answer => pc.setLocalDescription(answer))
+		.then(answer => console.log(JSON.stringify(pc.localDescription)));
+	//send the answer to the client
 } else {
-    btnShare.addEventListener('click', share);
+	btnShare.addEventListener('click', share);
 }
 
-
-
 function share() {
-    // channel = pc.createDataChannel('moves');
-    // channel.onopen = e => console.log("open");
-    // channel.onmessage = e => console.log(e.data)
-    // channel.onclose = e => console.log("closed");
+	// channelData = pc.createDataChannel('channelData');
+	// channelData.onopen = e => console.log('open');
+	// channelData.onmessage = e => console.log(e.data);
+	// channelData.onclose = e => console.log('closed');
 
-    pc.createOffer()
-        .then(o => {
-            console.log(window.location.origin + window.location.pathname + '?o=' + encodeURIComponent(JSON.stringify(o)));
-            pc.setLocalDescription(o);
-        });
-    
-    btnShare.style.display = 'none';
-    
-    btnStart.addEventListener('click', function() {
-        pc.setRemoteDescription(JSON.parse(eleCode.value))
-            .then(a => console.log("done"));
-        btnStart.style.display = 'none';
-        eleCode.style.display = 'none';
-    })
+	// pc.onicecandidate = e => console.log(pc.localDescription);
+
+	pc.createOffer().then(offer => {
+		pc.setLocalDescription(offer);
+		console.log(
+			window.location.origin +
+				window.location.pathname +
+				'?o=' +
+				encodeURIComponent(JSON.stringify(offer))
+		);
+	});
+
+	btnStart.addEventListener('click', function () {
+		pc.setRemoteDescription(JSON.parse(eleCode.value)).then(a =>
+			console.log('done')
+		);
+	});
 }
